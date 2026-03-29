ebfc892e7c6fb19cad4a1d491f09f66aa8121594
Felipe Quintiere <felipequintiere@gmail.com>
2026-03-29 18:57:36 -0300

    update

---
+++ 2.1.3/RectApp.java
@@ -0,0 +1,11 @@
+/*
+- Implemente os seguintes métodos:
+1. int area ()
+- deve retornar a área do retângulo
+2. void drag (int dx, int dy)
+- deve “arrastar” o objeto, ou seja somar o dx e dy a sua posição atual
+- Altere o método `print` para também exibir a área.
+- Inclua testes que garantam que os métodos estão corretos.
+- Respeite as assinaturas de método acima.
+- Não use `print` dentro dos métodos.
+*/
